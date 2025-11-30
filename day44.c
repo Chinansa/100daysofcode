96d2f434fa11289c52076f90084212905e505bd0
Chinansa <rchinansa@gmail.com>
2025-11-30 12:00:05 -0300

    Add string analysis and modification functionality

---
+++ day44.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int spaces = 0, digits = 0, special = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); // read string including spaces
+
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        char ch = str[i];
+
+        if (ch == ' ') {
+            spaces++;
+        } 
+        else if (ch >= '0' && ch <= '9') {
+            digits++;
+        } 
+        else if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            // Do nothing for letters
+        } 
+        else {
+            special++;
+        }
+    }
+
+    printf("Number of spaces: %d\n", spaces);
+    printf("Number of digits: %d\n", digits);
+    printf("Number of special characters: %d\n", special);
+
+    return 0;
+}
+
+
+
+#include <stdio.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin); 
+    for (int i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        if (str[i] == ' ') {
+            str[i] = '-';
+        }
+    }
+
+    printf("Modified string: %s\n", str);
+
+    return 0;
+}
