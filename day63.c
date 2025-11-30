bfe697c322d9192de67b4e3bf6e7905abc42e7fb
Chinansa <rchinansa@gmail.com>
2025-11-30 14:09:53 -0300

    Create day63.c

---
+++ day63.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+void sort(int arr[], int n) {
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, k;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    if(k < 1 || k > n) {
+        printf("Invalid value of k\n");
+        return 0;
+    }
+
+    sort(arr, n);  // Sort the array
+
+    printf("The %dth smallest element is: %d\n", k, arr[k - 1]);
+
+    return 0;
+}
