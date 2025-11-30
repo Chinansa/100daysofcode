e9f7d875ae3e85d94f896d71cb10b21e81d11ef5
Chinansa <rchinansa@gmail.com>
2025-11-30 14:31:59 -0300

    Create day82.c

---
+++ day82.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+enum TrafficLight {
+    RED,     // 0
+    YELLOW,  // 1
+    GREEN    // 2
+};
+
+int main() {
+    enum TrafficLight light;
+
+    for (light = RED; light <= GREEN; light++) {
+        switch (light) {
+            case RED:
+                printf("RED: Stop\n");
+                break;
+            case YELLOW:
+                printf("YELLOW: Wait\n");
+                break;
+            case GREEN:
+                printf("GREEN: Go\n");
+                break;
+        }
+    }
+
+    return 0;
+}
